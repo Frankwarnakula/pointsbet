b75a5ff36cb5c80303cf1fdd6a72d9591bf5816c
Frank Warnakula <frank.warnakula@gmail.com>
2025-09-09 21:43:09 +1000

    calling stringformatter from program

--- App/Program.cs
+++ App/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// call StringFormatter.ToCommaSepatatedList with some test data
+Console.WriteLine(
+    PointsBet.Backend.Online.Code.StringFormatter.ToCommaSepatatedList(
+        new[] { "apple", "banana", "cherry" }, "'"));
